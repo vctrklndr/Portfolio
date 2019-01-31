16c290f9de1b7549e8b254feb3bde3b872f604f5
vctrklndr <victor@nasa.se>
2019-01-31 09:49:31 +0100

    Updates

---
+++ precache-manifest.d60bbc7681aff7e67337a93a574d177b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e679eef757794b967d96004680f4052",
+    "url": "/React-Portfolio/static/media/mobergs_bistro.1e679eef.png"
+  },
+  {
+    "revision": "2855614f59711c3b74b1",
+    "url": "/React-Portfolio/static/css/main.fb3cc0b5.chunk.css"
+  },
+  {
+    "revision": "12515d38c4eb5e955f9c",
+    "url": "/React-Portfolio/static/js/1.12515d38.chunk.js"
+  },
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "a4be5ea1fdb869838ce4e4c5226acaa2",
+    "url": "/React-Portfolio/static/media/music-1.a4be5ea1.png"
+  },
+  {
+    "revision": "0b022f03e183253edea3368fa9d032a1",
+    "url": "/React-Portfolio/static/media/dashboard.0b022f03.jpg"
+  },
+  {
+    "revision": "cc2bd51e0e5b73be07a046e54205ee6b",
+    "url": "/React-Portfolio/static/media/ichores.cc2bd51e.png"
+  },
+  {
+    "revision": "2855614f59711c3b74b1",
+    "url": "/React-Portfolio/static/js/main.2855614f.chunk.js"
+  },
+  {
+    "revision": "ced89b22c619411c52e7e1cc8c03f8b5",
+    "url": "/React-Portfolio/static/media/punk_beer.ced89b22.png"
+  },
+  {
+    "revision": "5cd02dc80e088e5c789a205dc1231c3a",
+    "url": "/React-Portfolio/static/media/blob-1.5cd02dc8.svg"
+  },
+  {
+    "revision": "47bc204d3566445faa9e1f3d1a0a24df",
+    "url": "/React-Portfolio/static/media/Aften-Screen-Bold.47bc204d.ttf"
+  },
+  {
+    "revision": "1bb66f782eef699ae78687c5ad00768a",
+    "url": "/React-Portfolio/static/media/GT-Pressura-Mono-Bold.1bb66f78.ttf"
+  },
+  {
+    "revision": "c7f41f860339111eafbd2b6426de120d",
+    "url": "/React-Portfolio/static/media/GT-Pressura-Mono-Light.c7f41f86.ttf"
+  },
+  {
+    "revision": "4fcebad98bc1ed10f3985cb068497402",
+    "url": "/React-Portfolio/index.html"
+  }
+];
